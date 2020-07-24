3ee2655e08513495ec6e88a2bdcaf1f43c9cc4b5
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-07-24 16:11:23 -0700

    adding projects wrapper, project card, project.json

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,21 +1,45 @@
-import React from "react";
+import React, { useState} from "react";
+import Wrapper from "./Wrapper";
+import ProjectCard from "./ProjectCard";
+import projects from '../projects.json';
 
-const Projects = () => (
-  <div>
-    <h1>Projects</h1>
-    <p>
-      Donec a volutpat quam. Curabitur nec varius justo, sed rutrum ligula. Curabitur pellentesque
-      turpis sit amet eros iaculis, a mollis arcu dictum. Ut vel ante eget massa ornare placerat.
-      Etiam nisl orci, finibus sodales volutpat et, hendrerit ut dolor. Suspendisse porta dictum
-      nunc, sed pretium risus rutrum eget. Nam consequat, ligula in faucibus vestibulum, nisi justo
-      laoreet risus, luctus luctus mi lacus sit amet libero. Class aptent taciti sociosqu ad litora
-      torquent per conubia nostra, per inceptos himenaeos. Mauris pretium condimentum tellus eget
-      lobortis. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec placerat
-      accumsan mi, ut congue neque placerat eu. Donec nec ipsum in velit pellentesque vehicula sit
-      amet at augue. Maecenas aliquam bibendum congue. Pellentesque semper, lectus non ullamcorper
-      iaculis, est ligula suscipit velit, sed bibendum turpis dui in sapien.
-    </p>
-  </div>
-);
 
-export default Projects;
+
+
+function Project() {
+  // Using useState, declare a new state variable 'projectsList' and set it to the 'projects' array from 'projects.json'
+
+  const [projectsList, setProjectsList] = useState(projects);
+
+
+  const removeProject = (projectId) => {
+    const projectsListNewArr = projectsList.filter((project) => project.id !== projectId);
+    setProjectsList(projectsListNewArr)
+  }
+
+
+  
+ const renderProject = (project) => {
+   return (
+     <ProjectCard 
+     name={project.name} 
+     image={project.image} 
+     id={project.id} 
+     removeProject={removeProject}
+     />
+   )
+ }
+  //
+
+  return (
+    <Wrapper>
+      <h1 className="title">Project List</h1>
+
+        {projectsList.map((project) => (
+        renderProject(project)
+      ))}      
+    </Wrapper>
+  );
+}
+
+export default Project;
