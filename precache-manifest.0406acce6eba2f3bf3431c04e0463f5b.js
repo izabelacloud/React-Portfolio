169e2963d7addd4cd58cb7f57340f79df2d06da4
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-07-24 18:39:22 -0700

    Updates

---
+++ precache-manifest.0406acce6eba2f3bf3431c04e0463f5b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4bc24141cf6c8df732f6be9f3166b61",
+    "url": "/react-portfolio/index.html"
+  },
+  {
+    "revision": "92b5a6d6fdbe3e973e37",
+    "url": "/react-portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0c51169cb0be17bfa194",
+    "url": "/react-portfolio/static/css/main.e34cc0ae.chunk.css"
+  },
+  {
+    "revision": "92b5a6d6fdbe3e973e37",
+    "url": "/react-portfolio/static/js/2.4fc4c84a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-portfolio/static/js/2.4fc4c84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c51169cb0be17bfa194",
+    "url": "/react-portfolio/static/js/main.4b78ca96.chunk.js"
+  },
+  {
+    "revision": "aff33cef9dbb8787d6b3",
+    "url": "/react-portfolio/static/js/runtime-main.213b1880.js"
+  },
+  {
+    "revision": "354d28e9ff676c1e6b0d83a1b8c94e52",
+    "url": "/react-portfolio/static/media/cover-image.354d28e9.jpg"
+  },
+  {
+    "revision": "7c356c52b09febac972561319157fde1",
+    "url": "/react-portfolio/static/media/github2.7c356c52.png"
+  },
+  {
+    "revision": "57fd4550825b2b66049e45fb55ee7f73",
+    "url": "/react-portfolio/static/media/ip.57fd4550.jpg"
+  }
+]);
