d4b5b68429bd0b170398e3fd9147c41f2e029ff1
Izabela Petrovicova <i.petrovicova@gmail.com>
2020-07-25 15:42:16 -0700

    Updates

---
+++ precache-manifest.69e9a88f251cdf21b2f804159f4d054b.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2a7b9161d61c1fbcc39c1c3af58d8b0",
+    "url": "/ip-photo-port/index.html"
+  },
+  {
+    "revision": "92b5a6d6fdbe3e973e37",
+    "url": "/ip-photo-port/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "0a49ebcf925cc6ee1e54",
+    "url": "/ip-photo-port/static/css/main.c318d926.chunk.css"
+  },
+  {
+    "revision": "92b5a6d6fdbe3e973e37",
+    "url": "/ip-photo-port/static/js/2.4fc4c84a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ip-photo-port/static/js/2.4fc4c84a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a49ebcf925cc6ee1e54",
+    "url": "/ip-photo-port/static/js/main.cccf1dd5.chunk.js"
+  },
+  {
+    "revision": "668b911ef33b6bfc6729",
+    "url": "/ip-photo-port/static/js/runtime-main.234238b8.js"
+  },
+  {
+    "revision": "354d28e9ff676c1e6b0d83a1b8c94e52",
+    "url": "/ip-photo-port/static/media/cover-image.354d28e9.jpg"
+  },
+  {
+    "revision": "7c356c52b09febac972561319157fde1",
+    "url": "/ip-photo-port/static/media/github2.7c356c52.png"
+  },
+  {
+    "revision": "19b25fbadc22b40b280e4107eb61e882",
+    "url": "/ip-photo-port/static/media/ip2.19b25fba.jpg"
+  }
+]);
